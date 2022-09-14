e8d2ab14d6df86dd2dc95e4f7bdf9e3c27d0a652
Paula <pfr.blackcat@gmail.com>
2022-09-14 15:31:15 -0300

    first commit

---
+++ dartfolio/lib/dartfolio.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
+
+int calculate() {
+  print("Alencar A. Moreira");
+  print("Paula F. Rodrigues");
+  print("Rodrigo A. OLiveira");
+  int a = int.parse(stdin.readLineSync(encoding: utf8)!);
+  int b = int.parse(stdin.readLineSync(encoding: utf8)!);
+
+  // Return the user input
+  return a * b;
+}
---
+++ dartfolio/test/dartfolio_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartfolio/dartfolio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
